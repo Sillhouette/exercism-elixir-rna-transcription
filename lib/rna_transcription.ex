aadae8462411fecdad7c3730480042fcd33e6e33
Austin <austin@8thlight.com>
2020-01-28 10:05:36 -0800

    complete base exercise

--- lib/rna_transcription.ex
+++ lib/rna_transcription.ex
@@ -9,5 +9,14 @@ defmodule RnaTranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
+    map = %{
+      'G' => 'C',
+      'C' => 'G',
+      'T' => 'A',
+      'A' => 'U'
+    }
+    convert = fn x -> Map.get(map, [x]) end
+    map_conversion = Enum.map(dna, convert)
+    concat_conversion = Enum.concat(map_conversion)
   end
 end
